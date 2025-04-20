103a4e73914f253c9dac091f8465059570c07c03
minhdz-2005 <minhphung2102@gmail.com>
2025-04-20 17:42:06 +0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PBL3MAUIApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PBL3MAUIApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
