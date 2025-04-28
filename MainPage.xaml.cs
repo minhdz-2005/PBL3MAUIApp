0ec72f7d99f3f4b51fd4987d7f27bee9abea26ea
minhdz-2005 <minhphung2102@gmail.com>
2025-04-28 11:46:25 +0700

    LoginView

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,32 +1,13 @@
-﻿using System.Diagnostics;
-using System.Net.Http;
-using System.Text.Json;
-
-using PBL3MAUIApp.Models;
-using PBL3MAUIApp.Services;
-
+﻿
 namespace PBL3MAUIApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
-        private readonly CustomerService _accountService = new CustomerService();
         public MainPage()
         {
             InitializeComponent();
         }
-        protected override async void OnAppearing()
-        {
-            base.OnAppearing();
-
-            var accs = await _accountService.GetCustomersAsync();
-
-            foreach (var a in accs)
-            {
-                Debug.WriteLine($"Cus: {a.Id}, {a.Name}, {a.Username}, {a.PhoneNumber}");
-            }
-        }
+        
     }
 
 }
---
+++ Views/LoginView/LoginView.xaml.cs
@@ -0,0 +1,14 @@
+namespace PBL3MAUIApp.Views.LoginView;
+
+public partial class LoginView : ContentPage
+{
+    public LoginView()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnLoginClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//MainPage");
+    }
+}
